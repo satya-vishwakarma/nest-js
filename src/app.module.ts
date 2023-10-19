80b226189be93cf885b4b35d0465cfc38c1bfb86
satya-vishwakarma <satya9125@gmail.com>
2023-10-19 22:41:21 +0530

    feat:  formating  code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,13 @@
+import { MiddlewareConsumer, Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
+import { MongooseModule } from '@nestjs/mongoose'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
-import { MongooseModule } from '@nestjs/mongoose'
-import { UsersModule } from './users/users.module'
-import { MiddlewareConsumer, Module } from '@nestjs/common'
 import { AuthModule } from './auth/auth.module'
-import { RoleManagementModule } from './permissions/role-management/role-management.module'
 import { PermissionModule } from './permissions/permission/permission.module'
+import { RoleManagementModule } from './permissions/role-management/role-management.module'
 import { excludeRoutes } from './routes/excludeRoutes'
+import { UsersModule } from './users/users.module'
 
 @Module({
   imports: [
